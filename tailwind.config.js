0cfb9cdfe328bd48ca738e53fdf61101d94c93a1
Rahat Chowdhury <rchowdhury@simplehealth.com>
2021-08-06 23:56:23 -0400

    umm tailwind?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
 /* eslint-disable global-require */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
+  // eslint-disable-next-line global-require
   plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
